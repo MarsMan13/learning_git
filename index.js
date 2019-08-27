a9d78e7ddf881b90503d7fb0959477467832e10b
MarsMan13 <choigeon0513@gmail.com>
2019-08-27 16:30:12 +0900

    testBranch's modified

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const http = require('http'); const fs = require('fs'); const md = require('mark
 const port = 3000;
 //insert noting
 
+//testBranch
 function toHtml(markdown) { return ` <DOCTYPE html> <html lang="ko"> <head> <meta charset="utf-8"> </head> <body> ${md.render(markdown)} </body> </html>`; }
 http.createServer((request, response) => { console.log('Request received.');
 fs.readFile('view/README.md', 'utf8', (error, data) => { console.log('Read file - README.md'); response.writeHead(200, {'Content-Type': 'text/html'}); response.write(toHtml(data)); response.end(); });
